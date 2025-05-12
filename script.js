9e98d77f53ba26bf8091a5f2fa35cfffaa35a115
Sonal Tripathi <tripathisonal@gmail.com>
2025-05-12 15:57:13 +0530

    updated

---
+++ script.js
@@ -0,0 +1,25 @@
+const toggleBtn = document.querySelector('.toggle-menu button');
+const navMenu = document.querySelector('.menu ul');
+
+toggleBtn.addEventListener('click' , function(){
+    navMenu.classList.toggle('show');
+})
+
+const darkmodetoggle= document.querySelector('.dark-mode-toggle');
+const body= document.querySelector('body');
+
+darkmodetoggle.addEventListener('click',()=>{
+    body.classList.toggle('dark-mode');
+    updateDarkmodeicon();
+})
+
+function updateDarkmodeicon(){
+    const darkmodeicon=darkmodetoggle.querySelector('i')
+    if (body.classList.contains('dark-mode')) {
+        darkmodeicon.classList.remove('fa-moon');
+        darkmodeicon.classList.add('fa-sun');
+    }else{
+        darkmodeicon.classList.add('fa-moon');
+        darkmodeicon.classList.remove('fa-sun');
+    }
+}
